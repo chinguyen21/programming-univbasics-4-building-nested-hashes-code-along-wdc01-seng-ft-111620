029b40c0245564649ed03d6a8de1773baa77e533
Chi Nguyen <nguyen.thimai@northeastern.edu>
2020-10-20 18:20:31 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -4,12 +4,15 @@ def third_challenge
       :patriarch => {
         :name => "Lord Montague",
         :age => "53"
+      },
       :matriarch => {
         :name => "Lady Montague",
         :age => "54"
+      },
       :hero => {
          :name => "Romeo",
           :age => "15"
+      },
       :hero_friends => [
         ]
    }, 
